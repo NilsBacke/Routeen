52d88bd611868cad1f93141b0cde87f1840a04ef
NilsBacke <nils.backe@gmail.com>
2018-06-10 12:41:23 -0400

    added table view

--- Routeen/Controller/ViewController.swift
+++ Routeen/Controller/ViewController.swift
@@ -8,7 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var tasks = ["Brush teeth", "Wash face", "dasd", "d", "d", "d", "d", "d"]
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
